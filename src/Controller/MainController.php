16788ff0e51e7e9f565b9209aabc77bbc3417c48
Viktorovna1991 <viktorovna1991@gmail.com>
2022-06-16 00:00:58 +0200

    added new pages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,4 +20,58 @@ class MainController extends AbstractController
         //return new Response($html);
 
     }
+
+    /**
+     * @Route("/courses", name="app_courses")
+     */
+    public function courses(Environment $twigEnvironment)
+    {
+        return $this->render('page/courses.html.twig');
+
+    }
+
+    /**
+     * @Route("/teacher", name="app_teacher")
+     */
+    public function teacher(Environment $twigEnvironment)
+    {
+        return $this->render('page/teacher.html.twig');
+
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function about(Environment $twigEnvironment)
+    {
+        return $this->render('page/about.html.twig');
+
+    }
+
+    /**
+     * @Route("/pricing", name="app_pricing")
+     */
+    public function pricing(Environment $twigEnvironment)
+    {
+        return $this->render('page/pricing.html.twig');
+
+    }
+
+    /**
+     * @Route("/blog", name="app_blog")
+     */
+    public function blog(Environment $twigEnvironment)
+    {
+        return $this->render('page/blog.html.twig');
+
+    }
+
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function contact(Environment $twigEnvironment)
+    {
+        return $this->render('page/contact.html.twig');
+
+    }
 }
